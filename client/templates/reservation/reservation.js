bcbaa500fe890d167639a19bc482b1c9572b8ffb
ARBFAY <ARBAI.FAYCAL@GMAIL.COM>
2015-09-09 16:09:04 +0200

    avancement sur la partie back-end du formulaire de reservation : enregistrement de l'adresse de facturation

--- client/templates/reservation/reservation.js
+++ client/templates/reservation/reservation.js
@@ -29,5 +29,33 @@ Template.reservation.events({
 			
 			Router.go('/');
 			return false;
+	},
+	
+	"submit .payment" : function(event){
+		event.preventDefault();
+		
+		window.alert("Paiement procédé !");
+		
+		var prenom = event.target.lname.value;
+		var nom = event.target.fname.value;
+		var street = event.target.street.value;
+		var nr_appt = event.target.nr_appt.value;
+		var city = event.target.city.value;
+		var zip = event.target.zip_code.value;
+		var userId = Meteor.userId();
+
+		var facturationAdress = { fname : nom,
+								  lname : prenom,
+								  street : street,
+								  nr_appt : nr_appt,
+								  city : city,
+								  zip : zip
+								  };
+								  
+		Meteor.call("updateUser", {facturation : facturationAdress});
+		
+		window.alert("Adresse de facturation ajoutée");
+		
+		return false;
 	}
 });
